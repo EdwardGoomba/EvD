f1f995f969a2d63f279b29c152bdc9bd0e5ffdfd
EdwardGoomba <ask@edwardvdesign.com>
2018-08-16 08:07:37 -0400

    Refactor, add colors

--- src/views/LandingPage/Sections/SectionHelp.jsx
+++ src/views/LandingPage/Sections/SectionHelp.jsx
@@ -12,7 +12,6 @@ import CardBody from "components/Card/CardBody.jsx"
 import styles from "assets/jss/material-kit-pro-react/views/componentsSections/sectionCards.jsx"
 
 class SectionCards extends React.Component {
-
   render() {
     const { classes, ...rest } = this.props
     return (
@@ -21,76 +20,79 @@ class SectionCards extends React.Component {
           <GridContainer>
             <GridItem xs={12} sm={6} md={6} lg={4}>
               <Card color="info">
-                <CardBody color>
-                  <h5 className={classes.cardCategorySocialWhite}>
-                  </h5>
+                <CardBody>
+                  <h5 className={classes.cardCategorySocialWhite} />
                   <h4 className={classes.cardTitleWhite}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                    <h4>
                       Get an experienced guy who has worked on all aspects of the product phase numerous times. This helps streamline the process and draws on experience to eliminate issues as they start to pop up.
-                    </a>
+                    </h4>
                   </h4>
                 </CardBody>
               </Card>
 
-              <Card>
-                <CardBody>
-                  <h5 className={classes.cardCategory}>
-                  </h5>
-                  <h4 className={classes.cardTitle}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      Have someone who can convey design ideas both to marketing teams, and to developers eliminating the frustrations that come from a mismatched campaign and delivered product.
-                    </a>
-                  </h4>
-                </CardBody>
-              </Card>
-            </GridItem>
-            <GridItem xs={12} sm={6} md={6} lg={4}>
-              <Card>
-                <CardBody>
-                  <h5 className={classes.cardCategory}>
-                  </h5>
-                  <h4 className={classes.cardTitle}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      Have a reliable plan and steps in place instead of guessing and trying to invent the wheel with your existing team.
-                    </a>
-                  </h4>
-                </CardBody>
-              </Card>
               <Card color="success">
-                <CardBody color>
-                  <h5 className={classes.cardCategorySocialWhite}>
-                  </h5>
-                  <h4 className={classes.cardTitleWhite}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      Add a pretty rad designer to your team, who really loves what he does and the process behind it.
-                    </a>
-                  </h4>
-                </CardBody>
-              </Card>
-            </GridItem>
-            <GridItem xs={12} sm={6} md={6} lg={4}>
-              <Card color="warning">
-                <CardBody color>
-                  <h5 className={classes.cardCategorySocialWhite}>
-                  </h5>
-                  <h4 className={classes.cardTitleWhite}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      Don't have to sacrifice ideas as they come to the table and instead can focus on executing them, ultimately building more trust with your clients and customers.
-                    </a>
-                  </h4>
-                </CardBody>
-              </Card>
-              <Card>
                 <CardBody>
-                  <h5 className={classes.cardCategory}>
-                  </h5>
-                  <h4 className={classes.cardTitle}>
-                    <a href="#pablo" onClick={e => e.preventDefault()}>
-                      Might have to increase your coffee budget.. which leads to increasing office coffee consumption, and lets face it - you cant go wrong with more coffee. Its a win.
-                    </a>
+                  <h5 className={classes.cardCategorySocialWhite} />
+                  <h4 className={classes.cardTitleWhite}>
+                    <h4>
+                      Have someone who can convey design ideas both to marketing teams, and to developers eliminating the frustrations that come from a mismatched campaign and delivered product.
+                    </h4>
                   </h4>
                 </CardBody>
               </Card>
+
+            </GridItem>
+            
+            <GridItem xs={12} sm={6} md={6} lg={4}>
+
+              <Card color="warning">
+                <CardBody>
+                  <h5 className={classes.cardCategorySocialWhite} />
+                  <h4 className={classes.cardTitleWhite}>
+                    <h4>
+                      Have a reliable plan and steps in place instead of guessing and trying to invent the wheel with your existing team.
+                    </h4>
+                  </h4>
+                </CardBody>
+              </Card>
+
+              <Card color="danger">
+                <CardBody color>
+                  <h5 className={classes.cardCategorySocialWhite} />
+                  <h4 className={classes.cardTitleWhite}>
+                    <h4>
+                      Add a pretty rad designer to your team, who really loves what he does and the process behind it.
+                    </h4>
+                  </h4>
+                </CardBody>
+              </Card>
+
+            </GridItem>
+
+            <GridItem xs={12} sm={6} md={6} lg={4}>
+
+              <Card color="success">
+                <CardBody>
+                  <h5 className={classes.cardCategorySocialWhite} />
+                  <h4 className={classes.cardTitleWhite}>
+                    <h4>
+                      Don't have to sacrifice ideas as they come to the table and instead can focus on executing them, ultimately building more trust with your clients and customers.
+                    </h4>
+                  </h4>
+                </CardBody>
+              </Card>
+
+              <Card color="info">
+                <CardBody>
+                  <h5 className={classes.cardCategorySocialWhite} />
+                  <h4 className={classes.cardTitleWhite}>
+                    <h4>
+                      Might have to increase your coffee budget.. which leads to increasing office coffee consumption, and lets face it - you cant go wrong with more coffee. Its a win.
+                    </h4>
+                  </h4>
+                </CardBody>
+              </Card>
+
             </GridItem>
           </GridContainer>
         </div>
