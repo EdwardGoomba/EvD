c7d3abf0941d0d633be615bb4f6a718ef335e1f4
EdwardGoomba <ask@edwardvdesign.com>
2018-09-17 10:51:49 -0400

    Add about cards

--- src/views/HomePage/Sections/HelpSection.jsx
+++ src/views/HomePage/Sections/HelpSection.jsx
@@ -10,6 +10,8 @@ import GridItem from "components/Grid/GridItem"
 
 import productStyle from "assets/jss/material-kit-pro-react/views/landingPageSections/productStyle.jsx"
 
+import AboutCards from "../Sections/AboutCards"
+
 class HelpSection extends React.Component {
   render() {
     const { classes } = this.props
@@ -23,6 +25,7 @@ class HelpSection extends React.Component {
             </h3>
           </GridItem>
         </GridContainer>
+        <AboutCards />
       </div>
     )
   }
