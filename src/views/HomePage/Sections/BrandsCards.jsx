ab33ab775f54dec6f6f8b13df3bdcdf3613753c9
EdwardGoomba <ask@edwardvdesign.com>
2018-09-17 11:03:42 -0400

    Added cards to brands section

--- src/views/HomePage/Sections/BrandsCards.jsx
+++ src/views/HomePage/Sections/BrandsCards.jsx
@@ -7,7 +7,7 @@ import withStyles from "@material-ui/core/styles/withStyles"
 import GridContainer from "components/Grid/GridContainer.jsx"
 import GridItem from "components/Grid/GridItem.jsx"
 
-import typographyStyle from "assets/jss/material-kit-pro-react/views/componentsSections/typographyStyle.jsx"
+import styles from "assets/jss/material-kit-pro-react/views/componentsSections/sectionCards.jsx"
 
 import mynt from 'assets/img/brands/mynt.jpg'
 import renit from 'assets/img/brands/renit.jpg'
@@ -16,74 +16,75 @@ import warhammer from 'assets/img/brands/warhammer.jpg'
 
 class BrandsCards extends Component {
   render() {
-    const { classes } = this.props
+    const { classes, ...rest } = this.props
     return (
-      <div className={classes.container}>
-        <GridContainer>
-          <GridItem xs={12} sm={4} md={3} >
-            <img
-              src={mynt}
-              alt="..."
-              className={
-                classes.imgRaised +
-                " " +
-                classes.imgRounded +
-                " " +
-                classes.imgFluid
-              }
-            />
-            <h3 className={classes.title}>Mynt</h3>
-          </GridItem>
+      <div {...rest} className="cd-section" id="cards">
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={4} md={3}>
+              <img
+                src={mynt}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+              <h3 className={classes.title}>Mynt</h3>
+            </GridItem>
 
-          <GridItem xs={12} sm={4} md={3} >
-            <img
-              src={renit}
-              alt="..."
-              className={
-                classes.imgRaised +
-                " " +
-                classes.imgRounded +
-                " " +
-                classes.imgFluid
-              }
-            />
-            <h3 className={classes.title}>Renit</h3>
-          </GridItem>
+            <GridItem xs={12} sm={4} md={3}>
+              <img
+                src={renit}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+              <h3 className={classes.title}>Renit</h3>
+            </GridItem>
 
-          <GridItem xs={12} sm={4} md={3} >
-            <img
-              src={authentise}
-              alt="..."
-              className={
-                classes.imgRaised +
-                " " +
-                classes.imgRounded +
-                " " +
-                classes.imgFluid
-              }
-            />
-            <h3 className={classes.title}>Authentise</h3>
-          </GridItem>
+            <GridItem xs={12} sm={4} md={3} >
+              <img
+                src={authentise}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+              <h3 className={classes.title}>Authentise</h3>
+            </GridItem>
 
-          <GridItem xs={12} sm={4} md={3} >
-            <img
-              src={warhammer}
-              alt="..."
-              className={
-                classes.imgRaised +
-                " " +
-                classes.imgRounded +
-                " " +
-                classes.imgFluid
-              }
-            />
-            <h3 className={classes.title}>Warhammer</h3>
-          </GridItem>
-        </GridContainer>
+            <GridItem xs={12} sm={4} md={3} >
+              <img
+                src={warhammer}
+                alt="..."
+                className={
+                  classes.imgRaised +
+                  " " +
+                  classes.imgRounded +
+                  " " +
+                  classes.imgFluid
+                }
+              />
+              <h3 className={classes.title}>Warhammer</h3>
+            </GridItem>
+          </GridContainer>
+        </div>
       </div>
-
     )
   }
 }
 
-export default withStyles(typographyStyle)(BrandsCards)
+export default withStyles(styles)(BrandsCards)
